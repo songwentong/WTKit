62192c16f23615a737efe2c1ef896a0bb2e174c5
songwentong <songwentong@mobimagic.com>
2016-06-16 14:24:39 +0800

    注释掉输出

--- WTKit/WTKit/UIKitExtension.swift
+++ WTKit/WTKit/UIKitExtension.swift
@@ -399,7 +399,7 @@ public class ImageDownloadOperaion:NSOperation{
     
     
      deinit{
-        WTLog("deinit")
+//        WTLog("ImageDownloadOperaion deinit")
     }
     override public func main() {
         
