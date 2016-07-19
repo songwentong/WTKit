c869bfed29d03fea5fb9ff23344a4c796ed9102c
songwentong <songwentong@mobimagic.com>
2016-07-19 18:20:38 +0800

    越界判断

--- WTKit/WTKit/FoundationExtension.swift
+++ WTKit/WTKit/FoundationExtension.swift
@@ -694,6 +694,8 @@ extension UInt{
         }else{
             return number * wt_factorial(number: (number - 1))
         }
+        
+        
     }
 }
 extension Int{
@@ -704,7 +706,9 @@ extension Int{
         斐波那契数列,用递归实现的,容易出现栈溢出,推荐用颗粒化来做
      */
     public static func wt_fibonacci(number:UInt)->UInt{
-    
+        if number < 0 {
+            return 0
+        }
         if number <= 1 {
             return 1
         }else{
