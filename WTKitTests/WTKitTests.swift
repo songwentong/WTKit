c6eeafa8d4646f2a73c2d83c7a27e2cf5f29dd8d
songwentong <songwentong@mobimagic.com>
2016-07-22 10:24:37 +0800

    改下测试代码

--- WTKitTests/WTKitTests.swift
+++ WTKitTests/WTKitTests.swift
@@ -25,9 +25,19 @@ class WTKitTests: XCTestCase {
         testParseJSON()
     }
     func testURLSession(){
-        let url = "http://www.baidu.com"
+        let url = "https://www.apple.com"
         let task = URLSession.wt_dataTask(with: url) { (data, response, error) in
-            
+            //do somthing
+            print(data)
+        }
+        // set image
+        let imageView:UIImageView = UIImageView()
+        task.imageHandler = {(image:UIImage?,error:NSError?) in
+            imageView.image = image
+        }
+        // or get json
+        task.jsonHandler = {(anyObject:AnyObject?,error:NSError?) in
+            print(anyObject)
         }
         task.resume()
         
