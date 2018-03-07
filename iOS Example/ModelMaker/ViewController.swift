614e55da335976cb2b19aa95320a0ecde145f610
宋文通 <songwentong@mobimagic.com>
2018-03-07 13:57:17 +0800

    set text color

--- iOS Example/ModelMaker/ViewController.swift
+++ iOS Example/ModelMaker/ViewController.swift
@@ -150,7 +150,7 @@ extension ViewController:NSTextViewDelegate{
         
     }
     public func checkJSONText(){
-        
+        textView.textColor = NSColor.black
             if let data = textView.string.data(using: .utf8){
                 do {
                     let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
