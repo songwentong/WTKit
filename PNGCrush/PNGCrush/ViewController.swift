3a5380a2aaa4dcdb5c845ad37c1353ae3b2cbc33
宋文通 <songwentong@mobimagic.com>
2018-04-12 14:27:56 +0800

    add png crush

---
+++ PNGCrush/PNGCrush/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PNGCrush
+//
+//  Created by Wentong Song on 2018/4/12.
+//  Copyright © 2018年 Wentong Song. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
