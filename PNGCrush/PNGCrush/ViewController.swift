88afd4ca1c65625cb6c16884c5adc3e9dc307bb6
宋文通 <songwentong@mobimagic.com>
2018-04-12 15:27:22 +0800

    上传代码以后再做

--- PNGCrush/PNGCrush/ViewController.swift
+++ PNGCrush/PNGCrush/ViewController.swift
@@ -12,9 +12,32 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
+        var workPath = "/Users/songwentong/Desktop/images"
+        workPath = "/"
+        ///Users/wentongsong/Documents/cars 文稿
+        _ = findPaths(for: workPath)
+    }
+    func findPaths(for workPath:String) -> [String] {
+        guard let subpaths = FileManager.default.subpaths(atPath: workPath) else {
+            return []
+        }
+        var all:[String] = [String]()
+        var isDir:ObjCBool = false
+//        UnsafeMutablePointer.init()
+        ///       var number = 5
+        ///       let numberPointer = UnsafeMutablePointer<Int>(&number)
+        ///       // Accessing 'numberPointer' is undefined behavior.
+        let pointer = UnsafeMutablePointer<ObjCBool>(&isDir)
+        /*
+        for path in subpaths{
+            if FileManager.default.fileExists(atPath: path, isDirectory: pointer){
+                //dir
+                all.append(contentsOf: findPaths(for: path))
+            }else{
+                all.append(path)
+            }
+        }*/
+        return all
     }
 
     override var representedObject: Any? {
