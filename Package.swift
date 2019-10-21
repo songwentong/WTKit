ccc7ee589c100c9bd1644d3ce9e5307b6716dee5
songwentong <songwentong@hinterstellar.com>
2019-10-21 13:59:31 +0800

    SupportedPlatform

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "WTKit",
     platforms: [
-        .iOS(SupportedPlatform.IOSVersion.v9)
+        .iOS(SupportedPlatform.IOSVersion.v9),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
