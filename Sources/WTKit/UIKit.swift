f948af758930c3f615f104a190b61b94b60d308f
songwentong <songwentong@hinterstellar.com>
2019-10-21 17:18:35 +0800

    AlignLeftFlowLayout

--- Sources/WTKit/UIKit.swift
+++ Sources/WTKit/UIKit.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-#if !os(macOS)
 import UIKit
 public extension UIScreen{
     class func mainScreenWidth() -> CGFloat {
@@ -471,5 +470,24 @@ public func NSLibraryDirectory() -> String{
 public func NSLibraryCachesDirectory() -> String{
     return NSHomeDirectory() + "/Library" + "/Caches"
 }
-#endif
+
+public class AlignLeftFlowLayout: UICollectionViewFlowLayout {
+    override public func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let attributes = super.layoutAttributesForElements(in: rect)
+        
+        var leftMargin = sectionInset.left
+        var maxY: CGFloat = -1.0
+        attributes?.forEach { layoutAttribute in
+            if layoutAttribute.frame.origin.y >= maxY {
+                leftMargin = sectionInset.left
+            }
+            
+            layoutAttribute.frame.origin.x = leftMargin
+            
+            leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
+            maxY = max(layoutAttribute.frame.maxY , maxY)
+        }
+        return attributes
+    }
+}
 
