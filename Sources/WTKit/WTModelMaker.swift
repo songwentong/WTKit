43ab194a6ce7c1d373cf4269f93135d4c8061004
songwentong <songwentong@hinterstellar.com>
2019-11-22 10:59:10 +0800

    commonKeywords

--- Sources/WTKit/WTModelMaker.swift
+++ Sources/WTKit/WTModelMaker.swift
@@ -23,9 +23,22 @@ class MyClass {
 //    var @objc:Int = 1
 //    var default
 //    var fileprivate
+    var Int:Int = 1
+    var Double:Int = 1
+    var Float:Int = 1
+    var Set:Int = 1
+//    var subscript:Int = 1
+    var get:Int = 1
+//    var static:Int = 1
+//    var case:Int = 1
+    var print:Int = 1
+    var unowned:Int = 1
+    var weak:Int = 1
+//    var if:Int = 1
+//    var else:Int = 1
 }
 public class WTModelMaker {
-    public var commonKeywords:[String] = ["super","class","var","let","struct","func","private","public","return","import","protocol","default","open","Type","lazy","in","for","while","do","self","inout","@objc","open","fileprivate","default"]//常用的关键字命名修改,如有需要可以添加
+    public var commonKeywords:[String] = ["super","class","var","let","struct","func","private","public","return","import","protocol","default","open","Type","lazy","in","for","while","do","self","inout","@objc","open","fileprivate","default","subscript","static","case","if","else"]//常用的关键字命名修改,如有需要可以添加
     public var keywordsVarPrefix = ""//关键字属性的前缀,如有需要可以添加
     public var keywordsVarSuffix = "_var"//关键字属性的后缀,默认添加的是_var
     public var needQuestionMark:Bool = false //是否需要添加问号,来处理字段不存在的情况,true+问号?,否则不用加
