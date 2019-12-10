d11b3d4e421eebdefe34cb25c2bec907a50497a8
songwentong <songwentong@hinterstellar.com>
2019-12-10 11:31:22 +0800

    platforms

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "WTKit",
     platforms: [
         .iOS(SupportedPlatform.IOSVersion.v9),
+        .macOS(.v10_12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
