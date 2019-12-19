fdce0c9612e88e45b38119e0211fb6c543bc7d45
songwentong <songwentong@hinterstellar.com>
2019-12-19 11:20:16 +0800

    comment

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -5,3 +5,9 @@ import WTKitTests
 var tests = [XCTestCaseEntry]()
 tests += WTKitTests.allTests()
 XCTMain(tests)
+/*
+           513000   ^ixic
+ 2013.05   0.99     3325.35
+ 2019.12   3.129    8734.88
+           3.16     2.62
+ */
