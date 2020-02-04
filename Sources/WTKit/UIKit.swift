4a20e6f3baf88038855e077b856f1295a477ec78
songwentong <songwentong@hinterstellar.com>
2020-02-04 11:19:57 +0800

    wthud

--- Sources/WTKit/UIKit.swift
+++ Sources/WTKit/UIKit.swift
@@ -262,6 +262,10 @@ public extension UIView{
             layer.borderColor = newValue?.cgColor
         }
     }
+    func addSubViewAndTurnOffMask(_ view: UIView) {
+        addSubview(view)
+        view.turnOffMask()
+    }
     func turnOffMask()  {
         translatesAutoresizingMaskIntoConstraints = false
     }
@@ -314,39 +318,7 @@ public extension UIView{
         self.layer.mask = sl
     }
     
-    func showLoadingView() {
-        hideLoadingView()
-        let loadingView = LoadingView.init(frame: self.bounds)
-        addSubview(loadingView)
-    }
-    func hideLoadingView() {
-        let views = subviews.filter { (view) -> Bool in
-            if view is LoadingView{
-                return true
-            }
-            return false
-        }
-        for v in views{
-            v.removeFromSuperview()
-        }
-    }
-    func showTextTip(with string:String, hideDelay:TimeInterval = 2) {
-        hideTipTextView()
-        let tip = WTTextTip.init(frame: self.bounds)
-        tip.tipLabel.text = string
-        tip.isUserInteractionEnabled = false
-        addSubview(tip)
-        DispatchQueue.main.asyncAfterAfter(hideDelay) {
-            tip.removeFromSuperview()
-        }
-    }
-    func hideTipTextView() {
-        for v in subviews{
-            if v is WTTextTip{
-                v.removeFromSuperview()
-            }
-        }
-    }
+    
     @discardableResult
     func maskView(with cornerRadius:CGFloat) -> UIView {
         let v = UIView.init(frame: bounds)
@@ -621,15 +593,7 @@ public extension UIApplication{
         return keyWindowList.first
     }
     
-    static func showLoadingView() {
-        shared.windows.first?.showLoadingView()
-    }
-    static func hideLoadingView(){
-        shared.windows.first?.hideLoadingView()
-    }
-    static func showTextTip(with string:String, hideDelay:TimeInterval = 2) {
-        findKeyWindow()?.showTextTip(with: string, hideDelay: hideDelay)
-    }
+    
 }
 struct ImageLoadResult {
     let image:UIImage
@@ -799,72 +763,8 @@ public extension UIImage{
 public extension UIResponder{
     
 }
-// MARK: - LoadingView
-open class LoadingView: UIView {
-    var refreshIndicatorView = UIActivityIndicatorView.init(style: .whiteLarge)
-    var indicatorBGView:UIView = UIView.init()
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        confgView()
-    }
-    public required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        confgView()
-    }
-    open override class func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    func confgView() {
-        addSubview(indicatorBGView)
-        addSubview(refreshIndicatorView)
-        refreshIndicatorView.startAnimating()
-        self.isUserInteractionEnabled = false
-        indicatorBGView.translatesAutoresizingMaskIntoConstraints = false
-        indicatorBGView.backgroundColor = UIColor.black.withAlphaComponent(0.66)
-        indicatorBGView.layer.cornerRadius = 4
-        indicatorBGView.layer.masksToBounds = true
-        refreshIndicatorView.translatesAutoresizingMaskIntoConstraints = false
-        
-        
-        indicatorBGView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        indicatorBGView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        indicatorBGView.widthAnchor.constraint(equalToConstant: 75).isActive = true
-        indicatorBGView.heightAnchor.constraint(equalToConstant: 75).isActive = true
-        
-        refreshIndicatorView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        refreshIndicatorView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        
-    }
-}
-// MARK: - WTTextTip
-open class WTTextTip:UIView{
-    var tipLabel:UILabel = UILabel.init()
-    var tipLabelBGView:UIView = UIView.init()
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        configView()
-    }
-    public required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        configView()
-    }
-    func configView() {
-        addSubview(tipLabelBGView)
-        addSubview(tipLabel)
-        tipLabel.turnOffMask()
-        tipLabelBGView.turnOffMask()
-        tipLabelBGView.cornerRadius = 7
-        tipLabelBGView.backgroundColor = UIColor.black.withAlphaComponent(0.7)
-        tipLabel.font = .systemFont(ofSize: 15)
-        tipLabel.textColor = .white
-        tipLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        tipLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        tipLabelBGView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        tipLabelBGView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        tipLabelBGView.leadingAnchor.constraint(equalTo: tipLabel.leadingAnchor, constant: -15).isActive = true
-        tipLabelBGView.topAnchor.constraint(equalTo: tipLabel.topAnchor, constant: -18).isActive = true
-    }
-}
+
+
 // MARK: - UIScrollView
 public extension UIScrollView {
     /// SwifterSwift: Takes a snapshot of an entire ScrollView
