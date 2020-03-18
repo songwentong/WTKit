6551d17a4eff34d0d0ea96272e69c85763082a7a
songwentong <songwentong@hinterstellar.com>
2020-03-18 17:44:47 +0800

    shell

--- Sources/WTKit/Foundation.swift
+++ Sources/WTKit/Foundation.swift
@@ -943,19 +943,15 @@ public extension Collection where Element == String {
 }
 /**
  #xcode server build shell
- date
  git pull
  cd ..
- npm i
- npm run pack-ios
- date
- cd ios
  pod update
  agvtool new-version -all $XCS_INTEGRATION_NUMBER
  #release
- xcodebuild archive -workspace Stark.xcworkspace -scheme Stark -configuration Release -archivePath ~/Desktop/Stark.xcarchive
- xcodebuild -exportArchive -archivePath ~/Desktop/Stark.xcarchive -exportPath ~/Desktop/Stark.ipa -exportOptionsPlist ~/Desktop/ExportOptions.plist
- #upload fir ifneeded
+ #build and archive,use for not Xcode server 
+ #xcodebuild archive -workspace Stark.xcworkspace -scheme Stark -configuration Release -archivePath ~/Desktop/Stark.xcarchive
+ #xcodebuild -exportArchive -archivePath ~/Desktop/Stark.xcarchive -exportPath ~/Desktop/Stark.ipa -exportOptionsPlist ~/Desktop/ExportOptions.plist
+ #upload fir ifneeded,using fir-cli
  #fir publish ~/Desktop/Stark.ipa
  xcrun altool --validate-app -f $XCS_PRODUCT -u 275712575@qq.com -p swtpwd
  xcrun altool --upload-app -f $XCS_PRODUCT -u 275712575@qq.com -p swtpwd
