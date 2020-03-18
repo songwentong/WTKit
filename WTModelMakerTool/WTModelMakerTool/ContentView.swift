71081ba29d7ee92b69f7e8f4da7e040520ed039e
songwentong <songwentong@hinterstellar.com>
2020-03-18 14:48:30 +0800

    SwiftModelMakerTool

---
+++ WTModelMakerTool/WTModelMakerTool/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WTModelMakerTool
+//
+//  Created by 宋文通 on 2020/3/18.
+//  Copyright © 2020 宋文通. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
