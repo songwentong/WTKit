e5ea9ec3e79ce7e04a1da2bb95e377b80801daea
songwentong <songwentong@hinterstellar.com>
2020-03-18 17:35:15 +0800

    xcode server build shell

--- Sources/WTKit/Foundation.swift
+++ Sources/WTKit/Foundation.swift
@@ -941,3 +941,21 @@ public extension Collection where Element == String {
         }.joined(separator: ", ")
     }
 }
+/**
+ #xcode server build shell
+ date
+ git pull
+ cd ..
+ npm i
+ npm run pack-ios
+ date
+ cd ios
+ pod update
+ #release
+ xcodebuild archive -workspace Stark.xcworkspace -scheme Stark -configuration Release -archivePath ~/Desktop/Stark.xcarchive
+ xcodebuild -exportArchive -archivePath ~/Desktop/Stark.xcarchive -exportPath ~/Desktop/Stark.ipa -exportOptionsPlist ~/Desktop/ExportOptions.plist
+ #upload fir ifneeded
+ #fir publish ~/Desktop/Stark.ipa
+ xcrun altool --validate-app -f $XCS_PRODUCT -u 275712575@qq.com -p swtpwd
+ xcrun altool --upload-app -f $XCS_PRODUCT -u 275712575@qq.com -p swtpwd
+ */
