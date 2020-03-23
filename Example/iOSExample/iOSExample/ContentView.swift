688b7d83cb65c5a23350a5690c0d99a8e26db3ec
songwentong <songwentong@hinterstellar.com>
2020-03-23 13:56:46 +0800

    example

---
+++ Example/iOSExample/iOSExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSExample
+//
+//  Created by 宋文通 on 2020/3/23.
+//  Copyright © 2020 宋文通. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
