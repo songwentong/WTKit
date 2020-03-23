974e7ce676349021f0525f6093551bb783da218e
songwentong <songwentong@hinterstellar.com>
2020-03-23 16:59:58 +0800

    UI

--- Example/iOSExample/iOSExample/ContentView.swift
+++ Example/iOSExample/iOSExample/ContentView.swift
@@ -11,6 +11,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
+            .font(.largeTitle)
+            .fontWeight(.medium)
+            .padding(12.8)
+            
+            
+            
+            
+            
     }
 }
 
