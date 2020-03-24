3c870be4d5a9269e9a7456969685fecf29403f51
songwentong <songwentong@hinterstellar.com>
2020-03-24 09:57:29 +0800

    path

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "WTKit",
-            dependencies: []),
+            path: "Source"),
         .testTarget(
             name: "WTKitTests",
             dependencies: ["WTKit"]),
