d9f539b370c38a6939ebabfb8d6e56bbde7a6104
宋文通 <275712575@qq.com>
2022-02-26 13:52:35 +0800

    版本调整

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [
         .iOS(.v9),
         .macOS(.v10_12),
-        .tvOS(SupportedPlatform.TVOSVersion.v10)
+        .tvOS(.v10)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
