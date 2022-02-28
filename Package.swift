e277be2314acc3ba137697aa5b0a87699be0cfbb
宋文通 <275712575@qq.com>
2022-02-28 22:49:06 +0800

    path

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "WTKit",
-            dependencies: []),
+            dependencies: [], path: "Sources"),
         .testTarget(
             name: "WTKitTests",
             dependencies: ["WTKit"]),
