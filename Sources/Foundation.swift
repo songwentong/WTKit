10ab0c1065ee6e6bdf6679dfbf9852ccee3c2ebf
宋文通 <275712575@qq.com>
2022-04-17 22:51:49 +0800

    md5 string 用于文件保存的名字

--- Sources/Foundation.swift
+++ Sources/Foundation.swift
@@ -13,6 +13,9 @@ import CryptoKit
 #if canImport(Combine)
 import Combine
 #endif
+import var CommonCrypto.CC_MD5_DIGEST_LENGTH
+import func CommonCrypto.CC_MD5
+import typealias CommonCrypto.CC_LONG
 #if canImport(UIKit)
 import UIKit
 #endif
@@ -43,12 +46,27 @@ public extension String{
     var toBytes:[UInt8] {
         return Array(utf8)
     }
+    ///md5 string
+    var md5:String{
+        let length = Int(CC_MD5_DIGEST_LENGTH)
+        let messageData = utf8Data
+        var digestData = Data(count: length)
+
+        _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
+            messageData.withUnsafeBytes { messageBytes -> UInt8 in
+                if let messageBytesBaseAddress = messageBytes.baseAddress, let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
+                    let messageLength = CC_LONG(messageData.count)
+                    CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
+                }
+                return 0
+            }
+        }
+        return digestData.utf8String
+    }
+
     ///base64EncodedString
     var base64EncodedString:String{
-        if let data = data(using: .utf8){
-            return data.base64EncodedString()
-        }
-        return ""
+        utf8Data.base64EncodedString()
     }
 
     ///get localizedString from main Bundle
