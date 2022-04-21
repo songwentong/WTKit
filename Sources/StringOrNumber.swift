0cf5b6ab2e0ce662205d57df3ee8ee1f4b89c620
宋文通 <275712575@qq.com>
2022-04-21 16:09:47 +0800

    KeyedDecodingContainer 扩展

--- Sources/StringOrNumber.swift
+++ Sources/StringOrNumber.swift
@@ -6,6 +6,52 @@
 //
 
 import Foundation
+public extension KeyedDecodingContainer{
+    /**
+     Int解析,兼容字符串
+     */
+    func decodeToInt(forKey key: KeyedDecodingContainer<K>.Key) -> Int{
+        do {
+            
+            if let num = try decodeIfPresent(Int.self, forKey: key) {
+                print("aaaaaa")
+            }
+//            let num = try decode(Int.self, forKey: key)
+            return 1
+        } catch  {
+            let str = decodeToString(forKey: key)
+            return Int.init(str) ?? 0
+        }
+    }
+    ///Double解析,兼容字符串
+    func decodeToDouble(forKey key: KeyedDecodingContainer<K>.Key) -> Double{
+        do {
+            let num = try decode(Double.self, forKey: key)
+            return num
+        } catch  {
+            let str = decodeToString(forKey: key)
+            return Double.init(str) ?? 0
+        }
+    }
+    func decodeToBool(forKey key: KeyedDecodingContainer<K>.Key) -> Bool{
+        do {
+            let num = try decode(Bool.self, forKey: key)
+            return num
+        } catch  {
+            return false
+        }
+    }
+    ///String解析,兼容Double
+    func decodeToString(forKey key: KeyedDecodingContainer<K>.Key) -> String{
+        do {
+            let str = try decode(String.self, forKey: key)
+            return str
+        } catch  {
+            let num = decodeToDouble(forKey: key)
+            return String.init(num)
+        }
+    }
+}
 ///类型枚举器,用于JSON数据枚举,允许字段的数据是int,double和string
 ///配合Model Maker使用,可以让属性异常好用
 public enum StringOrNumber: Codable {
